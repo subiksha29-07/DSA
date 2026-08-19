07ba99ac0fc98d52ecd90961b2a19ca456e7187d
subiksha29-07 <subikshavs5126@gmail.com>
2026-08-19 19:22:09 +0530

    Time: 17 ms (11.18%) | Memory: 46.7 MB (15.62%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+    s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+    System.out.print(s);
+    int l = 0 ;
+    int r = s.length()-1;
+    while(l<=r){
+        if(s.charAt(l) != s.charAt(r)){
+            return false;
+        }
+        l++;
+        r--;
+    }
+
+     return true;   
+    }
+}
